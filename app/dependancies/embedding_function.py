9068906048f2d69a2c55d29bc9969f339de0927a
vorph <klimczak.mathieu@pm.me>
2022-05-24 15:12:53 +0000

    refactor: [skip ci] refactor functions, add utils.py

--- app/dependancies/embedding_function.py
+++ app/dependancies/embedding_function.py
@@ -1,10 +1,14 @@
-from functools import lru_cache
+from pathlib import Path
+from typing import List
 
 import numpy as np
 import onnxruntime as rt
 from loguru import logger
+from matplotlib import pyplot as plt
 from PIL import Image
+from sklearn.manifold import TSNE
 
+from app.config import settings
 from app.pydantic_models import EmbeddingsModel, Providers
 
 
@@ -28,20 +32,54 @@ class EmbeddingEngine:
         logger.info(f"{images.shape}")
 
         logits = self.loaded_model.run(["avg_pool"], {"input": images})
-        # logger.info(f"{type(logits)}")
 
         return logits[0]
 
+    def compute_tsne(self, logits: np.ndarray, n_components: int = 2) -> np.ndarray:
+        return TSNE(
+            n_components=2,
+            learning_rate="auto",
+            init="random",
+        ).fit_transform(logits)
 
-# model = "MobileViT-XXS_FPN_2021-10-13_17-37-15.onnx"
+    def plot(
+        self,
+        logits: np.ndarray,
+        images_paths: List[Path],
+        timestamp: str,
+    ) -> Path:
 
-# providers = ["CPUExecutionProvider"]
-# m = rt.InferenceSession(model, providers=providers)
-# onnx_pred = m.run(["avg_pool"], {"input": batch})
+        images_labels = [Path(image_path).parent.stem for image_path in images_paths]
+        labels_dict = {
+            label: idx for idx, label in enumerate(sorted(set(images_labels)))
+        }
+        tags = [labels_dict[image_label] for image_label in images_labels]
 
-# onnx_pred[0].shape
+        N = len(set(images_labels))
 
+        # define the colormap
+        cmap = plt.cm.jet
+        # extract all colors from the .jet map
+        cmaplist = [cmap(i) for i in range(cmap.N)]
+        # create the new map
+        cmap = cmap.from_list("Custom cmap", cmaplist, cmap.N)
 
-# image = Image.open(image_path).resize((1024, 1024))
-# print(f"{image.size}")
-# width, height = image.size
+        plt.figure(figsize=(10, 10))
+        scatter = plt.scatter(logits[:, 0], logits[:, 1], c=tags, cmap=cmap)
+
+        plt.title("tSNE scatterplot with ResNet50v2 preprocess")
+        plt.xlabel("Dimension 1")
+        plt.ylabel("Dimension 2")
+        plt.legend(
+            handles=scatter.legend_elements()[0],
+            labels=sorted(set(images_labels)),
+            title="Labels",
+        )
+
+        saved_image_path = Path(
+            f"{settings.scatterplots_dir}/tSNE_scatter_{timestamp}.png",
+        ).resolve()
+
+        plt.savefig(saved_image_path)
+
+        return saved_image_path
--- app/routes/embedding.py
+++ app/routes/embedding.py
@@ -1,29 +1,17 @@
-from io import BytesIO
-from pathlib import Path
-
 import arrow
-import numpy as np
-from fastapi import APIRouter, File, UploadFile, status
-from fastapi.responses import FileResponse, Response
-from loguru import logger
-from matplotlib import pyplot as plt
-from PIL import Image
-from sklearn.manifold import TSNE
+from fastapi import APIRouter, status
+from fastapi.responses import FileResponse
 
 from app.config import settings
 from app.dependancies.embedding_function import EmbeddingEngine
-from app.dependancies.utils import (
-    get_items_list,
-    load_image_into_numpy_array,
-    read_imagefile,
-)
-from app.pydantic_models import Embeddings, EmbeddingsModel, Extension, Providers
+from app.dependancies.utils import get_items_list
+from app.pydantic_models import EmbeddingsModel, Extension, Providers
 
 router = APIRouter()
 
 
 @router.get(
-    "/embeddings",
+    "/tsne",
     status_code=status.HTTP_200_OK,
     tags=["embedding"],
 )
@@ -44,46 +32,12 @@ async def get_cnn_embedding(
 
     logits = engine.infer(images_paths)
 
-    images_labels = [Path(image_path).parent.stem for image_path in images_paths]
-    labels_dict = {label: idx for idx, label in enumerate(sorted(set(images_labels)))}
-    tags = [labels_dict[image_label] for image_label in images_labels]
-    N = len(set(images_labels))
+    X_embedded = engine.compute_tsne(logits=logits)
 
-    # define the colormap
-    cmap = plt.cm.jet
-    # extract all colors from the .jet map
-    cmaplist = [cmap(i) for i in range(cmap.N)]
-    # create the new map
-    cmap = cmap.from_list("Custom cmap", cmaplist, cmap.N)
-
-    X_embedded = TSNE(
-        n_components=2,
-        learning_rate="auto",
-        init="random",
-    ).fit_transform(logits)
-
-    logger.info(f"{X_embedded.shape}")
-
-    scat = plt.scatter(X_embedded[:, 0], X_embedded[:, 1], c=tags, cmap=cmap)
-
-    plt.title("tSNE scatterplot with ResNet50v2 preprocess")
-    plt.xlabel("Dimension 1")
-    plt.ylabel("Dimension 2")
-    plt.legend(
-        handles=scat.legend_elements()[0],
-        labels=sorted(set(images_labels)),
-        title="species",
+    saved_image_path = engine.plot(
+        logits=X_embedded,
+        images_paths=images_paths,
+        timestamp=timestamp,
     )
-    # create the colorbar
-    # define the bins and normalize
-    bounds = np.linspace(0, N, N + 1)
-    cb = plt.colorbar(scat, spacing="proportional", ticks=bounds)
-    cb.set_label("Custom cbar")
-
-    saved_image_path = Path(
-        f"{settings.scatterplots_dir}/scatter_{timestamp}.png",
-    ).resolve()
-
-    plt.savefig(saved_image_path)
 
     return FileResponse(saved_image_path)
